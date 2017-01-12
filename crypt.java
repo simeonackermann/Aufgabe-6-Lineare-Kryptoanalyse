78bdb93efe68c246fdcab6916037977c684b2a66
Simeon Ackermann <s.ackermann@mail.de>
2017-01-12 10:28:10 +0100

    Implement numRounds. Change output

--- crypt.java
+++ crypt.java
@@ -5,40 +5,44 @@ import java.util.Random;
 
 public class crypt {
 	
-	// substitution
+	// S-Box substitution
 	int[] sBox = {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7};
 	// reverse substitution für entschlüsselung
 	int[] revSbox = {14, 5, 6, 10, 3, 15, 7, 9, 11, 0, 4, 1, 2, 8, 13, 12};
-	// 
-	int[] xValues = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	// Y belegungen für anwendung der maske auf approximationstabelle
 	int[] yValues = {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7};
-	// transpositions tabelle
+	// transpositions tabelle der DES verschlüsselung
 	int[] transTable = {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16};
 	// approximationstabelle
 	int[][] approxTable = new int[16][16];
-	// anzahl der paare
+	// anzahl der known pairs
 	int numKnown = 65536; // = 2^16
 	// known plaintexts
 	int[] knownP = new int[numKnown];
 	// known ciphertext
 	int[] knownC = new int[numKnown];
+	// anzahl der runden zur verschlüsselung
+	int numRounds = 4;
 	
 	void main(String[] args) {
-		// approximationstabelle berechnen
-		findApprox();
-		// approximtionstabelle anzeigen
-		showApprox();
-		System.out.println("Approx Tabelle" + Arrays.deepToString(approxTable));
-		
-		int inputApprox = 11;
-	    int outputApprox = 11;
+		System.out.println("Known Plaintext Paare erstellen...");
 	    fillKnowns();
-	    System.out.println("Known P: " + Arrays.toString(Arrays.copyOfRange(knownP, 0, 16)));
-	    System.out.println("Known C: " + Arrays.toString(Arrays.copyOfRange(knownC, 0, 16)));
+	    System.out.printf("Insgesamt %d Known Pairs erstellt.\n", knownP.length);
+	    System.out.printf("Klartext (erste 16): %s ...\n", Arrays.toString(Arrays.copyOfRange(knownP, 0, 16)));
+	    System.out.printf("Ciphertext (erste 16): %s ...\n\n", Arrays.toString(Arrays.copyOfRange(knownC, 0, 16)));
 	    
-	    return;
+	    System.out.println("Approximationstabelle erstellen...");
+	    // approximationstabelle berechnen
+	 	findApprox();
+	 	//System.out.println("Approximationstabelle" + Arrays.deepToString(approxTable));
+	 	// approximationstabelle anzeigen
+	 	showApprox();
 		
-	    /*int[] keyScore = new int[16];
+	    /*
+	    int inputApprox = 11;
+	    int outputApprox = 11;
+	    
+	    int[] keyScore = new int[16];
 	    int sofar1 = 0;
 	    
 	    System.out.printf("Linear Attack:  Using Linear Approximation = %d -> %d\n", inputApprox, outputApprox);
@@ -122,49 +126,57 @@ public class crypt {
 	        for(int a = 1; a < 16; a++) {
 	        	// input, jede zelle für 16 fälle testen 
 	            for(int e = 0; e < 16; e++) {
-	            	// wenn a & X1-4 == b & Y1-4 approx tabelle inkrementieren
+	            	// wenn a & X == b & Y approx tabelle inkrementieren
 	            	if (applyMask(e, a) == applyMask(yValues[e], b)) {
 	                    approxTable[a][b]++;
 	                }
 	            }
+	            // jeden eintrag minus 8 für normalisierung
+	            approxTable[a][b] -= 8;
 	        }
 		}
 		// jeden eintrag minus 8 für normalisierung
 		for(int b = 1; b < 16; b++) {
 			for(int a = 1; a < 16; a++) {
-				approxTable[a][b] -= 8;
+				//approxTable[a][b] -= 8;
 			}
 		}
 	}
 	
 	void showApprox() {
-	    System.out.println("Gute Lineare Approximationen:");
-	    for(int c = 1; c < 16; c++) {
-	        for(int d = 1; d < 16; d++) {
-	            if (Math.abs(approxTable[c][d]) >= 6) {
-	                System.out.printf("  %d : %d -> %d\n", approxTable[c][d], c, d);
-	            	//System.out.println(approxTable[c][d] + " ");
-	            }
+	    //System.out.println("Gute Lineare Approximationen:");
+		System.out.println("Approximationstabelle: (input mask \\ output mask)");
+		System.out.printf("\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\n");
+	    for(int c = 0; c < 16; c++) {
+	    	System.out.printf("%d\t", c);
+	        for(int d = 0; d < 16; d++) {
+	        	System.out.printf("%d\t", approxTable[c][d]);
+	            /*if (Math.abs(approxTable[c][d]) >= 6) {
+	                System.out.printf("  %d -> %d\t: %d\n", c, d, approxTable[c][d]);
+	            }*/
 	        }
+	        System.out.printf("\n");
 	    }
 	}
 	
 	void fillKnowns() {
 		Random rand = new Random(); 
-	    int subKey1 = Math.abs(rand.nextInt()) % numKnown;
-	    int subKey2 = Math.abs(rand.nextInt()) % numKnown;
-	    
-	    System.out.printf("Data Generator:  K1 = %d, K2 = %d\n", subKey1, subKey2);
+	    int[] roundKeys = new int[numRounds];
+	    for (int j = 0; j < numRounds; j++) {
+	    	roundKeys[j] = Math.abs(rand.nextInt()) % numKnown;
+        	System.out.printf("  Rundenschlüssel S %d: %d\n", j+1, roundKeys[j]);
+	    }
 	    
 	    for(int i = 0; i < numKnown; i++) {
 	    	// known plaintext erstellen, mit 2^16 mögl. werten
 	        knownP[i] = Math.abs(rand.nextInt()) % numKnown;
 	    
-	        //knownC[i] = roundFunc(roundFunc(knownP[i], subKey1), subKey2); 
-	        knownC[i] = roundFunc(knownP[i], subKey2);
-	    }    
-	    
-	    System.out.printf("Data Generator:  Generated %d Known Pairs\n\n", numKnown);
+	        // known ciphertext erstellen, dafür x runden durchlaufen
+	        knownC[i] = knownP[i];
+	        for (int j = 1; j <= numRounds; j++) {
+	        	knownC[i] = roundFunc(knownC[i], roundKeys[j-1]);
+			}
+	    }
 	}
 	
 	// input = known plaintext, subkey = rundenschlüssel
